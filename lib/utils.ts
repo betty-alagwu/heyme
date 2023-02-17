94aae1b2573695a41259f4e3f9cfcde0284ffc70
betty-alagwu <bettyalagwu@gmail.com>
2023-02-17 02:58:13 +0100

    adding new confetti firework feature

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,44 @@
 import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import confetti from 'canvas-confetti'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+
+export const runFireworks = () => {
+  var count = 100;
+var defaults = {
+  origin: { y: 0.7 }
+};
+
+function fire(particleRatio, opts) {
+  confetti(Object.assign({}, defaults, opts, {
+    particleCount: Math.floor(count * particleRatio)
+  }));
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+fire(0.2, {
+  spread: 60,
+});
+fire(0.35, {
+  spread: 150,
+  decay: 0.91,
+  scalar: 0.8
+});
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2
+});
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
 }
