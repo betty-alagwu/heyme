dad411f551f4f19e165147c4e58abbeaddc226b7
betty-alagwu <bettyalagwu@gmail.com>
2023-02-19 19:08:06 +0100

    fix: Convert share button to link

--- pages/success.tsx
+++ pages/success.tsx
@@ -8,6 +8,7 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { runFireworks } from '@/lib/utils';
 import Image from 'next/image';
 import emailGif from '../public/assets/emailGif.gif'
+import Link from 'next/link'
 
 
 
@@ -25,7 +26,7 @@ const Success = () => {
                 <section className="container justify-center text-center items-center pt-6  md:pb-7  
                 max-w-md border-slate-100 bg-white dark:bg-slate-800 dark:border-slate-800 shadow-md my-12 rounded-xl pb-6 	">
                     <div>
-                        <Image src={emailGif} alt="My GIF"  className='rounded-lg' />
+                        <Image src={emailGif} alt="My GIF" className='rounded-lg' />
                     </div>
                     <h1 className='font-bold pt-10 text-2xl'>Woo-hoo! </h1>
                     <p className='text-sm pt-5'> Your message to your future self is on its way to the future.</p>
@@ -33,18 +34,18 @@ const Success = () => {
                     </p>
 
                     <div className="mt-10 flex justify-center space-x-5 ">
-                        <button className=" rounded-full bg-blue-500 ">
+                        <Link className=" rounded-full bg-blue-500 " href={''}>
                             <GrFacebookOption className='m-3 text-white' size={20} />
                             <span className='sr-only'>Facebook</span>
-                        </button>
-                        <button className="bg-sky-800 rounded-full">
+                        </Link>
+                        <Link className="bg-sky-800 rounded-full" href={''}>
                             <FaLinkedinIn className='m-3 text-white' size={20} />
                             <span className='sr-only'>LinkedIn</span>
-                        </button>
-                        <button className="rounded-full bg-blue-400">
+                        </Link>
+                        <Link className="rounded-full bg-blue-400" href={''}>
                             <FaTwitter className='m-3 text-white' size={20} />
                             <span className='sr-only'>Twitter</span>
-                        </button>
+                        </Link>
                     </div>
                 </section>
             </Layout>
