8abac5b9e1688bd38dad3030e20237096d75c204
betty-alagwu <bettyalagwu@gmail.com>
2023-02-23 17:26:20 +0100

    fix: update the svg icon

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Link from "next/link"
+import Image from 'next/image';
 
 import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
@@ -14,6 +15,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import hello from '../public/assets/hello.png'
 
 interface MainNavProps {
   items?: NavItem[]
@@ -23,7 +25,8 @@ export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
       <Link href="/" className="hidden items-center space-x-2 md:flex">
-        <Icons.logo className="h-6 w-6" />
+      <Image src={hello} alt="My GIF" className='w-7 mr-3' />
+        {/* <Icons.logo className="h-6 w-6" /> */}
         <span className="hidden font-bold sm:inline-block">
           {siteConfig.name}
         </span>
@@ -53,8 +56,9 @@ export function MainNav({ items }: MainNavProps) {
             variant="ghost"
             className="-ml-4 text-base hover:bg-transparent focus:ring-0 md:hidden"
           >
-            <Icons.logo className="mr-2 h-4 w-4" />{" "}
-            <span className="font-bold">Menu</span>
+            {/* <Icons.logo className="mr-2 h-4 w-4" /> */}
+            <Image src={hello} alt="My GIF" className='w-7 mr-3' />
+            <span className="font-bold">Hey me</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent
