1f63181a51517f45319362639e4cd9bd95734d0f
betty-alagwu <bettyalagwu@gmail.com>
2023-02-27 11:51:45 +0100

    chore: update dispatched email

--- pages/api/dispatch.ts
+++ pages/api/dispatch.ts
@@ -1,6 +1,21 @@
+import Fs from "fs"
+import Path from "path"
 import { NextApiRequest, NextApiResponse } from "next"
 import { createMysqlConnection } from "@/utils/server"
 import { SendMailClient } from "zeptomail"
+
+function getEmailContent({ watchLink, greeting, body, outro }) {
+  let emailContent = Fs.readFileSync(
+    Path.resolve("public", "mails", "email.html")
+  ).toString()
+
+  emailContent = emailContent.replace("{{REPLACE_WATCH_LINK_HERE}}", watchLink)
+  emailContent = emailContent.replace("{{REPLACE_GREETING_HERE}}", greeting)
+  emailContent = emailContent.replace("{{REPLACE_BODY_HERE}}", body)
+  emailContent = emailContent.replace("{{REPLACE_OUTRO_HERE}}", outro)
+
+  return emailContent
+}
 
 function getTomorrowAndYesterdayDates() {
   let now = new Date()
@@ -78,7 +93,9 @@ export default async function handleDispatchEmails(
       continue
     }
 
-    await client.sendMail({
+    const isYourself = video.send_to === "yourself"
+
+    const res = await client.sendMail({
       bounce_address: "bounce@mails.heyme.io",
       from: {
         address: "betty@heyme.io",
@@ -91,13 +108,20 @@ export default async function handleDispatchEmails(
           },
         },
       ],
-      subject: `You've got a message from ${
-        video.send_to === "yourself" ? "yourself" : "someone"
-      }.`,
-      htmlbody: `
-      <div> <p> Hello. Thanks for this. Here's your message from the past. <a href="${process.env.SITE_URL}/watch/${video.id}">Watch your message now</a> </p> </div>
-      `,
+      subject: isYourself
+        ? `You sent yourself a message from the past. Check it out.`
+        : `Someone sent you a message from the past. Check it out.`,
+      htmlbody: getEmailContent({
+        watchLink: `${process.env.SITE_URL}/watch/${video.id}`,
+        greeting: isYourself
+          ? `Do you still remember sending yourself a message on ${video.created_at}?`
+          : `Someone sent you a message on ${video.created_at}`,
+        body: `This message was sent using our future message service called <a href="https://heyme.io" target="_blank">heyme</a>.`,
+        outro: "Thank you for using our service.",
+      }),
     })
+
+    console.log({ res: res.data })
 
     await updateVideoToSent(connection, video.id)
   }
