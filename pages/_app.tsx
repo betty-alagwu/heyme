8d63fac483179831e9d52b211aa41259c3e40dcb
betty-alagwu <betty-alagwu@gmail.com>
2023-03-10 08:34:43 +0100

    chore: setup vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app"
 import { Inter as FontSans } from "@next/font/google"
 import { ThemeProvider } from "next-themes"
+import { Analytics } from '@vercel/analytics/react'
 
 import "@/styles/globals.css"
 import Head from "next/head"
@@ -27,6 +28,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Component {...pageProps} />
       </ThemeProvider>
+      <Analytics />
     </>
   )
 }
