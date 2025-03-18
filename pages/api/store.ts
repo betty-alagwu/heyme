dfc6806174e582b6c6b7ab0fd4c0771f3ec05f4d
Betty Alagwu <bettyalagwu@gmail.com>
2025-03-18 20:12:14 +0100

    testing

--- pages/api/store.ts
+++ pages/api/store.ts
@@ -11,7 +11,7 @@ export async function handleStoreVideo(
 
   if (
     !data.video_url ||
-    !data.email ||
+    !data.email ,
     !data.send_at ||
     !data.send_to ||
     !data.created_at
@@ -20,11 +20,11 @@ export async function handleStoreVideo(
   }
 
   // connect to database
-  const connection = createMysqlConnection()
+  const connection = createMysqlConnection
 
   const result = await insertVideo(connection, data)
 
-  await connection.destroy()
+  await connection.
 
   response.json(result)
 }
